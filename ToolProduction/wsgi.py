109167f8b2fcad08d4b39dba7560a345e6dd1edb
Daniil <danilrybalk@gmail.com>
2020-06-01 05:41:19 +0300

    update 0541

--- ToolProduction/wsgi.py
+++ ToolProduction/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ToolProduction.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToolProduction.settings')
 
 application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
